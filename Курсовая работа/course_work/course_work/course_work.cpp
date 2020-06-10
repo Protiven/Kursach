fb7fd0b763943b5b0dcd81833924eb2e822e14ca
Protiven <39469340+Protiven@users.noreply.github.com>
2020-06-11 01:14:04 +0700

    up

--- Курсовая работа/course_work/course_work/course_work.cpp
+++ Курсовая работа/course_work/course_work/course_work.cpp
@@ -85,11 +85,11 @@ type_ GetGamma(type_ x, type_ y) {
 }
 
 type_ GetF(type_ x, type_ y, type_ t) {
-	return 3 ;
+	return -sin(t) ;
 }
 
 type_ GetIdeal(type_ x, type_ y, type_ t) {
-	return  pow(x, 2) + pow(y, 2) + t;
+	return  x + y + cos(t);
 }
 
 
@@ -351,7 +351,7 @@ void assembling(vector<vector<type_>> prev_resh, type_ time) {
 				// Вычисление правой части
 				type_ val = 0;
 				for (int z = 0; z < 4; z++)
-					val += gamma * C[i1][z] * prev_resh[][z];
+					val += gamma * C[i1][z] * prev_resh[z];
 				f[Index[i1]] += F[i1] + 1 / delta_time * val; 
 			}
 		}
